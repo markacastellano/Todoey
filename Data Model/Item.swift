cc3cbe4d1cdb26c952304bfaa3e3ff0a0d56bfd8
Mark A. Castellano <mark.a.castellano@mac-mini.fios-router.home>
2018-10-06 15:39:17 -0400

    Convert to MVC design pattern.

---
+++ Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mark A. Castellano on 9/29/18.
+//  Copyright © 2018 Mark A. Castellano. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    }
